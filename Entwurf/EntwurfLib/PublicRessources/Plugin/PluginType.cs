19fc115029fdcf1b4424fca5e417f44a1e34dea5
Artur Eckhart <artur.eckhart@gmail.com>
2012-06-21 09:26:46 +0200

    oqat.publicressources.plugin incodecomments

--- Entwurf/EntwurfLib/PublicRessources/Plugin/PluginType.cs
+++ Entwurf/EntwurfLib/PublicRessources/Plugin/PluginType.cs
@@ -1,12 +1,14 @@
-﻿//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-namespace Oqat.PublicRessources.Plugin
+﻿namespace Oqat.PublicRessources.Plugin
 {
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
 
+    /// <summary>
+    /// This enum contains all types of plugins known to the <see cref="PluginManager"/>.
+    /// A Plugin has to be of some of this types to be compatible to oqat.
+    /// </summary>
 	public enum PluginType : int
 	{
 		Filter,
