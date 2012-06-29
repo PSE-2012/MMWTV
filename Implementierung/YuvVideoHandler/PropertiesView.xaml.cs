616c7f6174f3363f0e7e3bf87dfa5b0b7159934a
Sebastian <sebastian.leidig@gmail.com>
2012-06-29 18:19:26 +0200

    YuvVideoHandler databinding PropertiesView <> VideoInfo

--- Implementierung/YuvVideoHandler/PropertiesView.xaml.cs
+++ Implementierung/YuvVideoHandler/PropertiesView.xaml.cs
@@ -14,14 +14,75 @@ using System.Windows.Shapes;
 
 namespace YuvVideoHandler
 {
-    /// <summary>
-    /// Interaktionslogik für UserControl1.xaml
-    /// </summary>
     public partial class PropertiesView : UserControl
     {
+        // Collection property used to fill the ComboBox with a list
+        List<ComboBoxYuvFormats> formatList;
+
         public PropertiesView()
         {
             InitializeComponent();
+
+            initFormatValues();
+            this.cb_format.ItemsSource = formatList;
+        }
+
+        /// <summary>
+        /// Set up the collection properties used to bind the ItemsSource 
+        /// properties to display the list of items in the dropdown lists.
+        /// </summary>
+        private void initFormatValues()
+        {
+            formatList = new List<ComboBoxYuvFormats>()
+            {
+                new ComboBoxYuvFormats(){ formatEnum = YuvFormat.YUV420, 
+			        formatString = "Yuv 4:2:0" },
+                new ComboBoxYuvFormats(){ formatEnum = YuvFormat.YUV444, 
+			        formatString = "Yuv 4:4:4" },
+                new ComboBoxYuvFormats(){ formatEnum = YuvFormat.YUV422, 
+			        formatString = "Yuv 4:2:2" },
+                new ComboBoxYuvFormats(){ formatEnum = YuvFormat.YUV411, 
+			        formatString = "Yuv 4:1:1" },
+            };
+        }
+        
+    }
+
+
+    /// <summary
+    /// This class provides us with an object to fill a ComboBox with
+    /// that can be bound to 'ViewModelEnum.Colors' enum fields in the binding
+    /// object while displaying a string values in the to user in the ComboBox.
+    /// </summary>
+    public class ComboBoxYuvFormats
+    {
+        YuvFormat _formatEnum;
+        string _formatString;
+
+        public YuvFormat formatEnum 
+        { 
+            get
+            {
+                return _formatEnum;
+            }
+            set
+            {
+                _formatEnum = value;
+            }
+        }
+        public string formatString
+        {
+            get
+            {
+                return _formatString;
+            }
+            set
+            {
+                _formatString = value;
+            }
         }
     }
+
+
+
 }
