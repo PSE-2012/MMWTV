949228a5682cbd041efd40fe9354618645a683d5
Sebastian <sebastian.leidig@gmail.com>
2012-07-02 16:46:59 +0200

    implemented VideoEventArgs

--- Implementierung/OQAT/PublicRessources/Model/VideoEventArgs.cs
+++ Implementierung/OQAT/PublicRessources/Model/VideoEventArgs.cs
@@ -13,25 +13,61 @@ namespace Oqat.PublicRessources.Model
 	/// </summary>
     public class VideoEventArgs : EventArgs
 	{
+        private Video _video;
+        private bool _isRefVid;
+
+        /// <summary>
+        /// Constructs a new instance of VideoEventArgs with the given video.
+        /// The property isRefVideo defaults to false.
+        /// </summary>
+        /// <param name="video">the video object for these eventArgs</param>
+        public VideoEventArgs(Video video)
+        {
+            this.video = video;
+            this.isRefVid = false;
+        }
+        /// <summary>
+        /// Constructs a new instance of VideoEventArgs with the given video and status.
+        /// </summary>
+        /// <param name="video">the video object for these eventArgs</param>
+        /// <param name="isRef">status wether this video is passed as a reference video</param>
+        public VideoEventArgs(Video video, bool isRef)
+        {
+            this.video = video;
+            this.isRefVid = isRef;
+        }
+
+
         /// <see cref="Video"/>
         /// <summary>
-        /// 
+        /// The video associated with this event.
         /// </summary>
 		public virtual Video video
 		{
-			get;
-			set;
+            get
+            {
+                return _video;
+            }
+            private set
+            {
+                _video = value;
+            }
 		}
 
         /// <summary>
-        /// Returns true if it's a reference video.
+        /// Returns true if it's a reference video. Default is false;
         /// </summary>
 		public virtual bool isRefVid
 		{
-			get;
-			set;
+            get
+            {
+                return _isRefVid;
+            }
+            private set
+            {
+                _isRefVid = value;
+            }
 		}
-
 	}
 }
 
