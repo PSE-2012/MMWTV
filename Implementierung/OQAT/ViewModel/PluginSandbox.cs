1ac82cf599f58e00c26d9269e2f614744b143c1e
Artur Eckhart <artur.eckhart@gmail.com>
2012-07-05 02:01:18 +0200

    implemented SandBox class to temporary load a single plugin reason: check for consistency within plugin folder

---
+++ Implementierung/OQAT/ViewModel/PluginSandbox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Oqat.PublicRessources.Plugin;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace Oqat.ViewModel
+{
+    class PluginSandbox
+    {
+        [Import(typeof(IPlugin))]
+        internal Lazy<IPlugin, IPluginMetadata> tmpPlugin
+        {
+            get;
+            private set;
+        }
+
+        
+        CompositionContainer tmpContainer;
+        internal PluginSandbox(string file) {
+
+            AssemblyCatalog tmpCatalog = new AssemblyCatalog(file);
+            tmpContainer = new CompositionContainer(tmpCatalog);
+            tmpContainer.ComposeParts(this.tmpPlugin);
+        }
+    }
+}
