d0b154fc3e69fd9aa56329d4755a888d7465d7c1
Artur Eckhart <artur.eckhart@gmail.com>
2012-07-06 11:20:10 +0200

    Can be used to pass string arguments within events

---
+++ Implementierung/OQAT/ViewModel/EntryEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oqat.ViewModel
+{
+    class EntryEventArgs : EventArgs
+    {
+        public EntryEventArgs(string entry)
+        {
+            this.entry = entry;
+        }
+        private string entry;
+        public string Entry {
+            get 
+            {
+                return entry; 
+            }
+        }
+    }
+}
