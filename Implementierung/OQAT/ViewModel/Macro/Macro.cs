81b8e82f628c5ecc9dd95b2d7c1ed70572d81e1d
Artur Eckhart <artur.eckhart@gmail.com>
2012-07-06 11:20:50 +0200

    IPlugin stubs

--- Implementierung/OQAT/ViewModel/Macro/Macro.cs
+++ Implementierung/OQAT/ViewModel/Macro/Macro.cs
@@ -36,6 +36,26 @@ namespace Oqat.ViewModel.Macro
         {
             throw new NotImplementedException();
         }
+
+        public void setParentControl(System.Windows.Controls.Panel parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<EventType, List<Delegate>> getEventHandlers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PublicRessources.Model.Memento getMemento()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setMemento(PublicRessources.Model.Memento memento)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
