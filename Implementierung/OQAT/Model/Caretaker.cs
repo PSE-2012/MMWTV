eae78bda2011f26065a535aeeb50b114b40422a8
Georg Ermantraut <georg.ermantraut@googlemail.com>
2012-07-07 12:56:57 +0200

    Caretaker

--- Implementierung/OQAT/Model/Caretaker.cs
+++ Implementierung/OQAT/Model/Caretaker.cs
@@ -10,6 +10,9 @@ namespace Oqat.Model
     using Oqat.PublicRessources.Model;
     using System.IO;
 
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+
 	/// <summary>
 	/// This class is the Model for the Caretaker, which is responsible for saving
     /// or loading the state of an object (Memento) to and from the hard disk drive,
@@ -17,7 +20,23 @@ namespace Oqat.Model
 	/// </summary>
     public class Caretaker
 	{
-        private static Caretaker cTaker;
+        /// <summary>
+        /// Used to lock while writeMemento doing his job
+        /// </summary>
+        private static object writeDisk = new Object();
+
+        /// <summary>
+        /// Allows only a single thread to enter the critical area,
+        /// which the lock block identifies, when no instance of Singleton has yet been created
+        /// </summary>
+        private static object syncRoot = new Object();
+
+
+        /// <summary>
+        /// The Caretaker Singelton instance.
+        /// </summary>
+        private static Caretaker _caretaker;
+
 		/// <summary>
 		/// Getter and setter methods for a Caretaker.
 		/// </summary>
@@ -25,100 +44,66 @@ namespace Oqat.Model
         {
             get
             {
-                if (cTaker == null)
-                    cTaker = new Caretaker();
-                return cTaker;
+                // The lock to prevent Caretaker to exist twice.
+                lock(syncRoot)
+                {
+                    //Check if Caretaker is already created.
+                    if (_caretaker == null)
+                    {
+                       _caretaker = new Caretaker();
+                    }
+                return _caretaker;
+                }
+            }
+            private set
+            {
+                caretaker = value;
             }
         }
 
         /// <summary>
-        /// A table of plugin names and a list of saved settings belonging to each of them.
-        /// </summary>
-		private Dictionary<string, List<Memento>> mementoTable
-		{
-			get;
-			set;
-		}
-
-        /// <summary>
-        /// Returns a new Caretaker.
-        /// </summary>
-        /// <returns></returns>
-		public static Caretaker getCaretaker()
-		{
-			throw new System.NotImplementedException();
-		}
-
-        /// <summary>
-        /// Removes saved settings of a plugin.
-        /// </summary>
-        /// <param name="origin"></param> Name of the plugin
-        /// <param name="nameMemento"></param> Reference to the name of the settings
-		public virtual void removeMemento(string origin, string nameMemento)
-		{
-			throw new System.NotImplementedException();
-		}
-
-        /// <summary>
-        /// Saves all newly created Mementos to the hard disk drive.
-        /// </summary>
-		public virtual void toDisk()
-		{
-			throw new System.NotImplementedException();
-		}
-
-        /// <summary>
         /// Creates a new Caretaker.
         /// </summary>
-		private Caretaker()
-		{
-		}
-
-        /// <summary>
-        /// Loads all Mementos located in a certain folder.
-        /// </summary>
-        /// <param name="mementoDir"></param> The folder to load the Mementos from.
-		public virtual void fromDisk(string mementoDir)
-		{
-			throw new System.NotImplementedException();
-		}
-
-        /// <summary>
-        /// Returns an array of all saved settings of a certain plugin.
-        /// </summary>
-        /// <param name="origin"></param> Plugin name
-        /// <returns></returns>
-		public virtual Memento[] getMementos(string origin)
-		{
-			throw new System.NotImplementedException();
-		}
-
-        /// <summary>
-        /// Returns the settings of a plugin referenced by a name.
-        /// </summary>
-        /// <param name="origin"></param> Plugin name
-        /// <param name="mementoname"></param> Name of the settings
-        /// <returns></returns>
-		public virtual Memento getMemento(string origin, string mementoname)
-		{
-			throw new System.NotImplementedException();
-		}
-
-        public virtual Memento getMemento(string path)
+        private Caretaker()
         {
-            throw new System.NotImplementedException();
         }
 
         /// <summary>
-        /// Adds new settings of a plugin.
+        /// Loads binary Memento file. If file doesn't exist, null will be returned.
         /// </summary>
-        /// <param name="origin"></param> Plugin name
-        /// <param name="memento"></param> Settings
-		public virtual void addMemento(string origin, Memento memento)
+        /// <param name="fileName"></param> The binary file to load.
+        public virtual Memento getMemento(string fileName)
 		{
-			throw new System.NotImplementedException();
+            Memento objectToSerialize = null;
+            if (File.Exists(fileName) && Path.GetExtension(fileName) == ".bin") //TODO: Rechte zum schreiben
+            {
+                //Reading files and creates a list of mementos
+                Stream stream = File.Open(fileName, FileMode.Open);
+                //using binary format
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                objectToSerialize = (Memento)bFormatter.Deserialize(stream);
+                stream.Close();
+            }
+            return objectToSerialize;
 		}
 
+        /// <summary>
+        /// Saves newly created Memento to the hard disk drive.
+        /// /// <param name="objectToSerialize"></param> The Memento
+        /// </summary>
+
+        public virtual void writeMemento(Memento objectToSerialize)
+        {
+            lock(writeDisk)
+            {
+                Stream stream = File.Open(objectToSerialize.mementoPath, FileMode.Create);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                bFormatter.Serialize(stream, objectToSerialize);
+                stream.Close();
+            }
+
+            
+        }
 	}
 }
 
--- Implementierung/OQAT/PublicRessources/Model/Memento.cs
+++ Implementierung/OQAT/PublicRessources/Model/Memento.cs
@@ -8,18 +8,29 @@ namespace Oqat.PublicRessources.Model
 	using System.Text;
     using System.IO;
 
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+
+
 	/// <summary>
 	/// This class is used for saving the internal state of an object, e.g. plugin settings.
 	/// </summary>
-    public class Memento
+    [Serializable()]
+    public class Memento : ISerializable
 	{
         /// <summary>
         /// Name of the Memento.
         /// </summary>
 		public virtual string name
 		{
-			get;
-			set;
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
 		}
 
         /// <summary>
@@ -27,8 +38,14 @@ namespace Oqat.PublicRessources.Model
         /// </summary>
 		public virtual object state
 		{
-			get;
-			set;
+            get
+            {
+                return this.state;
+            }
+            set
+            {
+                this.state = value;
+            }
 		}
 
         /// <summary>
@@ -36,8 +53,14 @@ namespace Oqat.PublicRessources.Model
         /// </summary>
 		public virtual string mementoPath
 		{
-			get;
-			set;
+            get
+            {
+                return this.mementoPath;
+            }
+            set
+            {
+                this.mementoPath = value;
+            }
 		}
 
         /// <summary>
@@ -45,7 +68,7 @@ namespace Oqat.PublicRessources.Model
         /// </summary>
         /// <param name="nameMemento"></param> Name of the Memento
         /// <param name="state"></param> Object state to be saved
-		public Memento(string nameMemento, object state)
+		public Memento(string nameMemento)
 		{
 		}
 
@@ -58,6 +81,20 @@ namespace Oqat.PublicRessources.Model
 		public Memento(string nameMemento, object state, string mementoPath)
 		{
 		}
+
+        public Memento(SerializationInfo info, StreamingContext ctxt)
+        {
+            this.name = (string)info.GetValue("Name", typeof(string));
+            this.state = (object)info.GetValue("State", typeof(object));
+            this.name = (string)info.GetValue("Memento Path", typeof(string));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("Name", this.name);
+            info.AddValue("State", this.state);
+            info.AddValue("Memento Path", this.mementoPath);
+        }
 
 	}
 }
