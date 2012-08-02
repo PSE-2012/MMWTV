f7d28d1691c29a2c5979331d55058b55a9b8d1d5
Artur Eckhart <artur.eckhart@gmail.com>
2012-08-02 12:05:38 +0200

    projectExplorer/project preBeta

--- Implementierung/OQAT/Model/Project.cs
+++ Implementierung/OQAT/Model/Project.cs
@@ -1,6 +1,4 @@
-﻿//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-namespace Oqat.Model
+﻿namespace Oqat.Model
 {
 	using System;
 	using System.Collections.Generic;
@@ -11,6 +9,7 @@ namespace Oqat.Model
     using System.IO;
     using Oqat.PublicRessources.Plugin;
     using Oqat.PublicRessources.Model;
+    using Oqat.ViewModel;
     using System.Collections.ObjectModel;
 
 	/// <summary>
@@ -26,8 +25,15 @@ namespace Oqat.Model
 			get;
 			private set;
 		}
+
+
         private int _unusedId = 0;
         private object idLock;
+
+        /// <summary>
+        /// Each SmartNode has an unambiguos id
+        /// by which it can be identified.
+        /// </summary>
         public int unusedId {
             get
             {
@@ -36,10 +42,16 @@ namespace Oqat.Model
                 }
             }
         }
+        /// <summary>
+        /// In order to reduce the searching overhead
+        /// for a given SmartNode, each SmartNode
+        /// is stored (the reference) within the
+        /// smartIndex where the key is an unambiguos id.
+        /// </summary>
         private Dictionary<int, SmartNode> smartIndex;
 
 		/// <summary>
-		/// Description of the project.
+		/// Project description, made by the user.
 		/// </summary>
         public string description
 		{
@@ -67,13 +79,13 @@ namespace Oqat.Model
         }
 
         /// <summary>
-        /// 
+        /// Constructs a new project.
         /// </summary>
-        /// <param name="name"></param> Name of the project.
+        /// <param name="name"></param> Project name.
         /// <param name="path"></param> Path to the project folder.
         /// <param name="vidList"></param> List of videos belonging to the project.
         /// <param name="description"></param> Description of the project.
-		public Project(string name, string path, string description, List<Video> vidList = null)
+		internal Project(string name, string path, string description, List<Video> vidList = null)
 		{
             this.idLock = new Object();
             this.name = name;
@@ -90,14 +102,65 @@ namespace Oqat.Model
                 }
             }
 		}
-        public void addNode(Video vid, int idFather) {
+
+        /// <summary>
+        /// Constructs a new project according to 
+        /// the state object withing the given memento.
+        /// </summary>
+        /// <remarks>The state object within the given memento has to 
+        /// be of the type <see cref="ProjectProperties"/> or a
+        /// ArgumentException will be thrown.</remarks>
+        /// <param name="mem">Memento to extract new settings from.</param>
+        internal Project(Memento mem)
+        {
+          
+            try
+            {
+               var prProperties = (ProjectProperties)mem.state;
+               if (prProperties.complete)
+                   this.setMemento(mem);
+               else
+                   throw new ArgumentException("Given memento does not contain all requered properties.");
+            }
+            catch (InvalidCastException exc)
+            {
+                throw new ArgumentException("State object within the given memento: " + mem.name + " is not of the type"
+                    + " ProejctProperties.", exc);
+            }
+
+ 
+
+        }
+
+
+        /// <summary>
+        /// Adds a new SmartNode to the SmartTree.
+        /// </summary>
+        /// <param name="vid">Video object to put into SmartNode. If
+        /// given video is not referenced, a ArgumentExcetion will
+        /// be raised.</param>
+        /// <param name="idFather">Father id of the new SmartNode.
+        /// If father id is smaller 0, the SmartNode will
+        /// be added on Top of the SmartTree.If father id is
+        /// greater 0 but does not exist in the current SmartTree structure
+        /// a ArgumentException will be raised.</param>
+        /// 
+        /// 
+        internal void addNode(Video vid, int idFather) {
+            // Check arguments for validity.
+            if (vid == null)
+                throw new ArgumentException("Given Video object is null.");
+            else if ((idFather >= 0) && !smartIndex.ContainsKey(idFather))
+                throw new ArgumentException("Given father id is unknown.");
+
+            // Process valid arguments.
             SmartNode newChild = new SmartNode(vid, unusedId, idFather);
             smartIndex.Add(newChild.id, newChild);
             SmartNode father;
             if (idFather >= 0)
             {
                 smartIndex.TryGetValue(idFather, out father);
-                father.smartTree.Add(newChild);
+                    father.smartTree.Add(newChild);
             }
             else
             {
@@ -105,48 +168,271 @@ namespace Oqat.Model
             }
         }
 
-        public void rmNode(int id, bool force)
+        /// <summary>
+        /// Remove SmartNode with
+        /// the given id from the SmartTree.
+        /// </summary>
+        /// <param name="id">Id of the SmartNode to remove.</param>
+        /// <param name="force">If force is true, all children will be removed, else
+        /// children will be passed to next father node.</param>
+        internal void rmNode(int id, bool force)
         {
+
+            if (!smartIndex.ContainsKey(id))
+                throw new ArgumentException("Given id is unknown.");
+
             SmartNode toRmNode;
             smartIndex.TryGetValue(id, out toRmNode);
-            if (toRmNode != null)
-            {
-                SmartNode child;
-                smartIndex.TryGetValue(id, out child);
-                SmartNode father;
-                smartIndex.TryGetValue(child.idFather, out father);
-                if ((toRmNode.smartTree.Count > 0) & !force)
-                {
-                    foreach (SmartNode i in child.smartTree)
+
+            if ((toRmNode.idFather >= 0) && !smartIndex.ContainsKey(toRmNode.idFather))
+                throw new ArgumentException("Given father id is unknown.");
+
+            SmartNode father;
+            smartIndex.TryGetValue(toRmNode.idFather, out father);
+
+            if (father != null)
+                father.smartTree.Remove(toRmNode);
+            else
+                this.smartTree.Remove(toRmNode);
+
+            smartIndex.Remove(id);
+
+            if ((toRmNode.smartTree.Count > 0) & !force)
+             {
+                 foreach (SmartNode i in toRmNode.smartTree)
                     {
                         i.idFather = father.id;
                     }
-                    father.smartTree.Concat(child.smartTree);
+                 if (father == null)
+                     smartTree.Concat(toRmNode.smartTree);
+                 else
+                     father.smartTree.Concat(toRmNode.smartTree);
 
-                }
-                father.smartTree.Remove(child);
-                smartIndex.Remove(id);
-            }
+             }
         }
         /// <summary>
-        /// Loads a saved project from the hard disk drive.
+        /// Return a memento for the current project.
         /// </summary>
         /// <returns></returns>
 		public virtual Memento getMemento()
 		{
-			throw new System.NotImplementedException();
+            return new Memento(this.name, 
+                new ProjectProperties(name, description,path_Project, smartIndex, smartTree, _unusedId), 
+                this.path_Project + this.name + "_Project.oqat");
 		}
 
         /// <summary>
-        /// Saves the project to the hard disk drive so it can be loaded again during
-        /// another execution of OQAT.
+        /// Sets the current properties to those within
+        /// the given memento state object.
         /// </summary>
-        /// <param name="memento"></param>
-		public virtual void setMemento(Memento memento)
+        /// <param name="memento">Memento to get the new properties from.</param>
+		public virtual void setMemento(Memento mem)
 		{
-			throw new System.NotImplementedException();
+            ProjectProperties prProperties;
+            try
+            {
+                prProperties = (ProjectProperties)mem.state;
+            }
+            catch (InvalidCastException exc)
+            {
+                throw new ArgumentException("State object within the given memento: " + mem.name + " is not of the type"
+                    + " ProejctProperties.", exc);
+            }
+
+            name = String.IsNullOrEmpty(prProperties.name) ? name : prProperties.name;
+            description = String.IsNullOrEmpty(prProperties.description) ? description : prProperties.description;
+
+            // ProjectProperties checked path for validity at construction time.
+            path_Project = String.IsNullOrEmpty(prProperties.path_Project) ? path_Project : prProperties.description;
+
+            if (prProperties.newTrees)
+            {
+                _unusedId = prProperties._unusedId;
+                smartIndex.Clear();
+                smartIndex.Concat(prProperties.smartIndex);
+
+                smartTree.Clear();
+                smartTree.Concat(prProperties.smartTree);
+            }
+            
+
 		}
 
 	}
+
+
+    /// <summary>
+    /// This class can be stored within a <see cref="Memento"/> and
+    /// be retrieved by a Project to restore or change settings respectively
+    /// smartNodes. All values within this class are either null or valid.
+    /// </summary>
+    internal class ProjectProperties
+    {
+        /// <summary>
+        /// Indicates whether all properties requered
+        /// to consruct a new Project instance are valid.
+        /// </summary>
+        internal bool complete = false;
+
+        /// <summary>
+        /// Project name.
+        /// </summary>
+        internal string name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Project description.
+        /// </summary>
+        internal string description
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// This bool indicates whether smartIndex, smartTree and
+        /// _unusedId are valid.
+        /// </summary>
+        internal bool newTrees = false;
+
+
+        /// <summary>
+        /// The smallest unused id, were unused
+        /// means that there is no element within
+        /// the smartIndex or the smartTree with
+        /// the same id.
+        /// </summary>
+        internal int _unusedId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Contains same smartNodes as 
+        /// smartTree but arranges them 
+        /// by an id.
+        /// </summary>
+        internal Dictionary<int, SmartNode> smartIndex
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Contains smae smartNodes as
+        /// smartIndex but arranges them
+        /// as a tree.
+        /// </summary>
+        internal ObservableCollection<SmartNode> smartTree
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Project path. It
+        /// is made sure that this path
+        /// is valid.
+        /// </summary>
+        internal string path_Project
+        {
+            get;
+            private set;
+        }
+
+
+        internal ProjectProperties(string name, string description, string path_Project,
+            Dictionary<int, SmartNode> smartIndex, ObservableCollection<SmartNode> smartTree, int _unusedId) 
+        {
+            this.name = String.IsNullOrWhiteSpace(name) ? null : name;
+            this.description = String.IsNullOrWhiteSpace(description) ? null : description;
+
+            try
+            {
+                new FileInfo(path_Project);
+                this.path_Project = path_Project;
+            }
+            catch (Exception exc)
+            {
+                throw new ArgumentException("Given project path" + path_Project + " is invalid.", exc);
+            }
+
+            newTrees = smartConsistency(smartIndex, smartTree, _unusedId);
+            if (newTrees)
+            {
+                this._unusedId = _unusedId;
+                this.smartTree = smartTree;
+                this.smartIndex = smartIndex;
+            }
+
+            complete = newTrees && String.IsNullOrWhiteSpace(name) && 
+                        String.IsNullOrWhiteSpace(description) && 
+                            String.IsNullOrWhiteSpace(path_Project);
+
+        }
+
+        /// <summary>
+        /// Checks given parameters for consistency. 
+        /// </summary>
+        /// <param name="smartIndex"></param>
+        /// <param name="smartTree"></param>
+        /// <param name="_unusedId"></param>
+        /// <returns>Returns false if
+        /// incosistencies were found.</returns>
+        private bool smartConsistency(Dictionary<int, SmartNode> smartIndex, ObservableCollection<SmartNode> smartTree, int _unusedId) {
+           
+            bool consistent = true;
+            if ((smartTree != null) && (smartIndex != null))
+            {
+
+
+                if (smartIndex.ContainsKey(_unusedId))
+                    consistent = false;
+
+                int smartTreeCount = 0;
+
+                if (consistent)
+                    foreach (SmartNode i in smartTree)
+                    {
+
+                        if ((i.id == _unusedId) 
+                            || !smartIndex.ContainsKey(i.id)
+                            || i.idFather >= 0)
+                        {
+                            consistent = false;
+                            break;
+                        }
+
+                        smartTreeCount++;
+                        foreach (SmartNode k in i.smartTree)
+                        {
+                            if ((k.id == _unusedId) 
+                                || !smartIndex.ContainsKey(k.id)
+                                || k.idFather != i.idFather)
+                            {
+                                consistent = false;
+                                break;
+                            }
+
+                            smartTreeCount++;
+                        }
+                    }
+                if (smartIndex.Count != smartTreeCount)
+                    consistent = false;
+            }
+            else
+            {
+                consistent = false;
+            }
+            
+            return consistent;
+        }
+
+    }
 }
 
--- Implementierung/OQAT/ViewModel/VM_ProjectExplorer.xaml.cs
+++ Implementierung/OQAT/ViewModel/VM_ProjectExplorer.xaml.cs
@@ -8,8 +8,13 @@
     using Oqat.Model;
     using Oqat.PublicRessources.Model;
     using System.Windows.Controls;
-using System.Collections.ObjectModel;
-
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using System.Windows;
+    using Microsoft.WindowsAPICodePack.Controls.WindowsPresentationFoundation;
+    using System.Windows.Data;
+    using Microsoft.WindowsAPICodePack.Shell;
+    using System.IO;
     /// <summary>
     /// This class is mainly responsible to sync the SmartTree(GUI) with the SmartNodes (Model) and
     /// raising the right events if user wants to delete/play a video.
@@ -30,10 +35,21 @@ using System.Collections.ObjectModel;
 		public VM_ProjectExplorer(Project project, Panel parent)
 		{
             InitializeComponent();
+
+            // projectExplorer
             this.project = project;
-            myTreeView.DataContext = project.smartTree;
+            smartTreeExplorer.DataContext = project.smartTree;
+
+            // fileExplorer
+            fileExp.ViewMode = Microsoft.WindowsAPICodePack.Controls.ExplorerBrowserViewMode.List;
+            this.Loaded += new RoutedEventHandler(ExpBrows_Loaded);
             parent.Children.Add(this);
 		}
+        void ExpBrows_Loaded(object sender, RoutedEventArgs e)
+        {
+            fileExp.ExplorerBrowserControl.Navigate((ShellObject)KnownFolders.Desktop);
+        }
+
 
         /// <summary>
         /// If the user clicks a entry out of the SmartTree the <see cref="IVideoInfo"/> object contents (size, history..)
@@ -42,6 +58,15 @@ using System.Collections.ObjectModel;
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void onVideoClick(object sender, VideoEventArgs e) { }
+
+        private void onSmartNodeSelect(object sender, RoutedPropertyChangedEventArgs<object> e) 
+        {
+            SmartNode selected = (SmartNode)e.NewValue;
+
+
+        }
+
+
 
 	}
 }
