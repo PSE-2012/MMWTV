951d0ab77ddb084eb30341563a482b5829aa30a8
Artur Eckhart <artur.eckhart@gmail.com>
2012-08-05 19:48:05 +0200

    made serializable

--- Implementierung/OqatPublicResources/Plugin/MacroEntry.cs
+++ Implementierung/OqatPublicResources/Plugin/MacroEntry.cs
@@ -4,12 +4,14 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
+    using System.Runtime.Serialization;
 
     /// <summary>
     /// This class represents a particular plugin and its memento wich enables a way to store such
     /// processing informatin within a PF_Macro or PM_Metric object.
     /// See MacroEntryMetric and MacroEntryFilter for usecases.
     /// </summary>
+    [Serializable]
 	public abstract class MacroEntry
 	{
 		private string pluginName
@@ -24,6 +26,17 @@
 			set;
 		}
 
-	}
+        //public MacroEntry(SerializationInfo info, StreamingContext context)
+        //{
+        //    this.pluginName = (string)info.GetValue("pluginName", typeof(string));
+        //    this.mementoName = (string)info.GetValue("mementoName", typeof(string));
+        //}
+
+        //public void GetObjectData(SerializationInfo info, StreamingContext context)
+        //{
+        //    info.AddValue("pluginName", this.pluginName);
+        //    info.AddValue("mementoName", this.mementoName);
+        //}
+    }
 }
 
