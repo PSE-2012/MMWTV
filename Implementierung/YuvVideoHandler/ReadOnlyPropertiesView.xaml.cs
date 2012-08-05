9b2fabc2afcf7bc4790ac7beddc7c024d46e82a3
Artur Eckhart <artur.eckhart@gmail.com>
2012-08-05 19:45:36 +0200

    preImplementationo of the VideoInfo control, to display below the smartTree

---
+++ Implementierung/YuvVideoHandler/ReadOnlyPropertiesView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PS_YuvVideoHandler
+{
+    /// <summary>
+    /// This class is a readOnly version of the <see cref="PropertiesView"/> class, it can
+    /// be used to display general informations about a video.
+    /// </summary>
+    public partial class ReadOnlyPropertiesView : UserControl
+    {
+        public ReadOnlyPropertiesView(YuvVideoInfo yuvInfo)
+        {
+            InitializeComponent();
+            if (yuvInfo != null)
+                throw new NullReferenceException("Given YuvVideoInfo object is not initialized.");
+            this.DataContext = yuvInfo;
+        }
+    }
+}
