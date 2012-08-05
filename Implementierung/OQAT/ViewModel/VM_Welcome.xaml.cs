58bd70c5e3550199b595057789e63c058095ef12
Artur Eckhart <artur.eckhart@gmail.com>
2012-08-05 19:41:51 +0200

    Fancy style, prepreBeta implementation

--- Implementierung/OQAT/ViewModel/VM_Welcome.xaml.cs
+++ Implementierung/OQAT/ViewModel/VM_Welcome.xaml.cs
@@ -8,6 +8,8 @@
 
     using Oqat.PublicRessources.Model;
     using System.Windows.Controls;
+    using System.Windows;
+    using Microsoft.Win32;
 
     /// <summary>
     /// This component is displayed whenever no project is open.
@@ -33,9 +35,43 @@
         /// <summary>
         /// Constructor.
         /// </summary>
-        /// <param name="parent">A panel where VM_Welcome can place itself.</param>
-        public VM_Welcome(Panel parent)
+        public VM_Welcome()
         {
+            InitializeComponent();
+        }
+
+        private void newPrjCreate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var prOpen = new VM_ProjectOpenDialog();
+            prOpen.Owner = Window.GetWindow(this);
+            Nullable<bool> result = prOpen.ShowDialog();
+            if ((result != null) & (bool)result)
+                PluginManager.pluginManager.raiseEvent(PublicRessources.Plugin.EventType.newProjectCreated, new ProjectEventArgs(prOpen.project));
+        }
+
+        private void exPrjOpen_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".oqatPrj";
+            dlg.Filter = "OQAT projects (.oqatPrj)|*.oqatPrj";
+
+            Nullable<bool> result = dlg.ShowDialog();
+            Memento exPrjMem;
+            if (result == true)
+            {
+                exPrjMem = Caretaker.caretaker.getMemento(dlg.FileName);
+                Project exPrj = new Project(exPrjMem);
+                PluginManager.pluginManager.raiseEvent(PublicRessources.Plugin.EventType.newProjectCreated,
+                    new ProjectEventArgs(exPrj));
+            }
+            else
+            {
+                string corruptPrj = "";
+                string caption = "Couldnt open project.";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(corruptPrj, caption, button, icon);
+            }
         }
 
     }
