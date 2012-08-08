8688de52b58c92a161aa096554a579298692b580
AlexanderMonev <bcclan@mail.bg>
2012-08-08 02:16:18 +0200

    Implemented basic functions of MacroMetric

--- Implementierung/OQAT/ViewModel/Macro/PM_MacroMetric.cs
+++ Implementierung/OQAT/ViewModel/Macro/PM_MacroMetric.cs
@@ -3,12 +3,14 @@
 namespace Oqat.ViewModel.Macro
 {
 	using Oqat.PublicRessources.Plugin;
+    using Oqat.PublicRessources.Model;
+    using Oqat.Model;
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
     using System.Windows.Controls;
-
+    using System.Data;
     using System.Drawing;
 
     /// <summary>
@@ -33,6 +35,91 @@ namespace Oqat.ViewModel.Macro
             throw new NotImplementedException();
         }
 
+        public PM_MacroMetric()
+        {
+            macroQueue = new DataTable();
+            macroQueue.Columns.Add("Metric Name", typeof(String));
+            macroQueue.Columns.Add("Memento Name", typeof(String));
+        }
+
+        private int BUFFERSIZE;
+        private int totalFrames;
+        private int i;
+        private IVideoHandler refHand;
+        private IVideoHandler procHand;
+        private IVideoHandler[] resHand;
+        private IMetricOqat currentPlugin;
+        private Memento currentMemento;
+        private AnalysisInfo analyseInfo;
+        private System.Drawing.Bitmap[] refFrames;
+        private System.Drawing.Bitmap[] procFrames;
+        private System.Drawing.Bitmap[] resultFrames;
+
+        public void init(Video vidRef, Video vidProc, Video[] vidResult)
+        {
+            IVideoHandler refHand = vidRef.handler;
+            IVideoHandler procHand = vidProc.handler;
+            IVideoHandler[] resHand = new IVideoHandler[vidResult.Length];
+            for (int k = 0; k < vidResult.Length; k++)
+            {
+                resHand[k] = vidResult[k].handler;
+            }
+            BUFFERSIZE = 255;
+            totalFrames = vidRef.vidInfo.frameCount; // TODO: do not allow the analyse if vidRef and vidProc have got a different frame count
+            i = 0;
+        }
+
+        public void analyse(Video vidRef, Video vidProc, Video[] vidResult)
+        {
+            // Warning: the method, implemented this way, does not support having another macrometric inside the list of metrics
+            for (int m = 0; m < macroQueue.Rows.Count; m++)
+            {
+                DataRow c = macroQueue.Rows[m];
+                currentPlugin = (IMetricOqat)PluginManager.pluginManager.getPlugin<IPlugin>((String)c["Metric Name"]);
+                currentMemento = PluginManager.pluginManager.getMemento((String)c["Metric Name"], (String)c["Memento Name"]);
+                while (i < totalFrames)
+                {
+                    if ((i + BUFFERSIZE - totalFrames) > 0)
+                    {
+                        refFrames = refHand.getFrames(i, totalFrames - i);
+                        procFrames = procHand.getFrames(i, totalFrames - i);
+                        resultFrames = new System.Drawing.Bitmap[totalFrames - i];
+                    }
+                    else
+                    {
+                        refFrames = refHand.getFrames(i, BUFFERSIZE);
+                        procFrames = procHand.getFrames(i, BUFFERSIZE);
+                        resultFrames = new System.Drawing.Bitmap[BUFFERSIZE];
+                    }
+                    refFrames = refHand.getFrames(i, BUFFERSIZE);
+                    procFrames = procHand.getFrames(i, BUFFERSIZE);
+                    int arraycount = refFrames.Count();
+                    for (int j = 0; j < arraycount; j++) // iterate over all frames to be analysed
+                    {
+                        currentPlugin.setMemento(currentMemento);
+                        analyseInfo = currentPlugin.analyse(refFrames[j], procFrames[j]); // result of the current analysis
+                        resultFrames[j] = analyseInfo.frame; // write the result frames to buffer
+                        vidResult[m].frameMetricValue[i] = analyseInfo.values; // sets frameMetricValue for the result video of the current metric
+                    }
+                    resHand[m].writeFrames(i, resultFrames); // write the result frames to disk
+                    i += BUFFERSIZE;
+                }
+                i = 0;
+            }     
+            currentPlugin = null;
+            currentMemento = null;
+            refFrames = null;
+            procFrames = null;
+            refHand = null;
+            resHand = null;
+            procHand = null;
+            refFrames = null;
+            procFrames = null;
+            analyseInfo = null;
+            resultFrames = null;
+            // TODO: event analyse finished
+        }
+
         public string namePlugin
         {
             get
@@ -55,11 +142,6 @@ namespace Oqat.ViewModel.Macro
             {
                 throw new NotImplementedException();
             }
-        }
-
-        public void setParentControl(System.Windows.Controls.Panel parent)
-        {
-            throw new NotImplementedException();
         }
 
         public Dictionary<EventType, List<Delegate>> getEventHandlers()
