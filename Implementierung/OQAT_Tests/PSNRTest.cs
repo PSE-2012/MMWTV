8956909a973fbb7dc35fabafa1e6fe747c3db14f
Georg Ermantraut <georg.ermantraut@googlemail.com>
2012-08-30 12:01:26 +0200

    PSNR Test

--- Implementierung/OQAT_Tests/PSNRTest.cs
+++ Implementierung/OQAT_Tests/PSNRTest.cs
@@ -1,4 +1,4 @@
-﻿using PM_MSE;
+﻿using PM_PSNR;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Drawing;
@@ -12,11 +12,11 @@ namespace OQAT_Tests
     
     
     /// <summary>
-    ///Dies ist eine Testklasse für "MSETest" und soll
-    ///alle MSETest Komponententests enthalten.
+    ///Dies ist eine Testklasse für "PSNRTest" und soll
+    ///alle PSNRTest Komponententests enthalten.
     ///</summary>
     [TestClass()]
-    public class MSETest
+    public class PSNRTest
     {
         private static Bitmap refBitmap;
         private static Bitmap procBitmap;
@@ -49,7 +49,7 @@ namespace OQAT_Tests
         [ClassInitialize()]
         public static void MyClassInitialize(TestContext testContext)
         {
-            MSE testMSE = new MSE();
+            PSNR testMSE = new PSNR();
             refBitmap = new Bitmap(100, 100);
             for (int height = 0; height < refBitmap.Height; height++)
             {
@@ -109,22 +109,22 @@ namespace OQAT_Tests
 
 
         /// <summary>
-        ///Ein Test für "MSE-Konstruktor"
+        ///Test "PSNR-Constructor"
         ///</summary>
         [TestMethod()]
-        public void MSEConstructorTest()
+        public void PSNRConstructorTest()
         {
-            MSE target = new MSE();
-            Assert.IsTrue(target is MSE, "The returned object is not a valid MSE instance.");
+            PSNR target = new PSNR();
+            Assert.IsTrue(target is PSNR, "The returned object is not a valid PSNR instance.");
         }
 
         /// <summary>
-        ///Ein Test für "analyse"
+        ///Test "analyse"
         ///</summary>
         [TestMethod()]
         public void analyseTest()
         {
-            MSE target = new MSE();
+            PSNR target = new PSNR();
             Bitmap frameRef = refBitmap;
             Bitmap frameProc = procBitmap;
             AnalysisInfo expected = analysisInfo;
@@ -147,97 +147,16 @@ namespace OQAT_Tests
         }
 
         /// <summary>
-        ///Test "analyse": Bitmap's null
-        ///</summary>
-        [TestMethod()]
-        public void analyseTest_null()
-        {
-            MSE target = new MSE();
-            Bitmap frameRef = null;
-            Bitmap frameProc = null;
-            AnalysisInfo actual;
-            actual = target.analyse(frameRef, frameProc);
-            Assert.IsNull(actual, "analyse can not handle null.");
-        }
-
-        /// <summary>
-        ///Test "analyse": Bitmap's empty
-        ///</summary>
-        [TestMethod()]
-        public void analyseTest_empty()
-        {
-            MSE target = new MSE();
-            Bitmap frameRef = new Bitmap(15, 15);
-            Bitmap frameProc = new Bitmap(15, 15);
-            AnalysisInfo actual;
-            actual = target.analyse(frameRef, frameProc);
-            Assert.IsTrue(actual is AnalysisInfo, "analyse can not handle empty Bitmaps.");
-        }
-
-        /// <summary>
-        ///Test "getMemento": start Mememento is set.
+        ///Test "getMemento"
         ///</summary>
         [TestMethod()]
         public void getMementoTest()
         {
-            MSE target = new MSE();
-            Memento expected = new Memento("PM_MSE", 0);
+            PSNR target = new PSNR();
+            Memento expected = new Memento("testMemento", target);
             Memento actual;
             actual = target.getMemento();
             Assert.AreEqual(expected.state, actual.state);
-            Assert.AreEqual(expected.name, actual.name);
-        }
-
-        /// <summary>
-        ///Ein Test für "localize"
-        ///</summary>
-        [TestMethod()]
-        [DeploymentItem("PM_MSE.dll")]
-        public void localizeTest()
-        {
-            MSE_Accessor target = new MSE_Accessor();
-            string s = "care"; // TODO: Passenden Wert initialisieren
-            target.localize(s);
-        }
-
-        /// <summary>
-        ///Test "setMemento"
-        ///</summary>
-        [TestMethod()]
-        public void setMementoTest()
-        {
-            MSE_Accessor target = new MSE_Accessor();
-            Memento memento = new Memento("MSE", 2);
-            target.setMemento(memento);
-            Assert.AreEqual(target.propertiesView.getRb(), 2, "propertiesView Radio Button could not be set.");
-        }
-
-        /// <summary>
-        ///Test "setMemento": Object is not type int
-        ///</summary>
-        [TestMethod()]
-        public void setMementoTest_notInt()
-        {
-            MSE target = new MSE();
-            List<Memento> memList = new List<Memento>();
-            Memento memento = new Memento("MEMLIST", memList);
-            target.setMemento(memento);
-        }
-
-        /// <summary>
-        ///Test "setMemento": Object has to be in range [0, 3]
-        ///</summary>
-        [TestMethod()]
-        public void setMementoTest_range()
-        {
-            MSE_Accessor target = new MSE_Accessor();
-            Memento memento = new Memento("RANGE", -1);
-            target.setMemento(memento);
-            Assert.IsTrue(target.propertiesView.getRb() > -1, "Invailed Range for MSE. Range [0, 3]");
-
-            memento = new Memento("RANGE", 4);
-            target.setMemento(memento);
-            Assert.IsTrue(target.propertiesView.getRb() < 4, "Invailed Range for MSE. Range [0, 3]");
         }
 
         /// <summary>
@@ -246,8 +165,8 @@ namespace OQAT_Tests
         [TestMethod()]
         public void namePluginTest()
         {
-            MSE target = new MSE();
-            string expected = "PM_MSE";
+            PSNR target = new PSNR();
+            string expected = "PM_PSNR";
             string actual;
             target.namePlugin = expected;
             actual = target.namePlugin;
@@ -260,7 +179,7 @@ namespace OQAT_Tests
         [TestMethod()]
         public void propertyViewTest()
         {
-            MSE target = new MSE();
+            PSNR target = new PSNR();
             UserControl actual;
             actual = target.propertyView;
         }
@@ -271,7 +190,7 @@ namespace OQAT_Tests
         [TestMethod()]
         public void typeTest()
         {
-            MSE target = new MSE();
+            PSNR target = new PSNR();
             PluginType expected = PluginType.IMetricOqat;
             PluginType actual;
             target.type = expected;
