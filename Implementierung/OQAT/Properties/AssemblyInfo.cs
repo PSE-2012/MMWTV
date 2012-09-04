687ebcaec2083178e0cd9d07e0b28abd38bdaca2
Sebastian <sebastian.leidig@gmail.com>
2012-09-04 09:57:14 +0200

    updated WindowOqatInfo

--- Implementierung/OQAT/Properties/AssemblyInfo.cs
+++ Implementierung/OQAT/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
-[assembly: AssemblyFileVersion("0.8.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
